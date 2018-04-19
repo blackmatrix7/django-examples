dde0d985eabbd8a7785bac2d3e652b2d54d76eb9
Matrix <codecolor@outlook.com>
2018-04-19 10:36:53 +0800

    增加配置文件

---
+++ settings/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2018/4/19 上午10:31
+# @Author: BlackMatrix
+# @Site: https://github.com/blackmatrix7
+# @File: __init__.py
+# @Software: PyCharm
+
+__author__ = 'blackmatrix'
+
+if __name__ == '__main__':
+    pass
