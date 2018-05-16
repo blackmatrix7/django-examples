aa7c0a1d98979c6b71feeda894592f9a3fa9fd47
Matrix <codecolor@outlook.com>
2018-05-16 22:35:05 +0800

    新建model

--- django_multidb/boss/models.py
+++ django_multidb/boss/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Driver(models.Model):
+
+    name = models.CharField('老板姓名', max_length=12)
+    age = models.IntegerField('老板年龄')
--- django_multidb/client/models.py
+++ django_multidb/client/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Driver(models.Model):
+
+    name = models.CharField('乘客姓名', max_length=12)
+    age = models.IntegerField('乘客年龄')
