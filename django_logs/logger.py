8d731f962f26b7e8ff48d2d27e6bee911ff6b102
Matrix <codecolor@outlook.com>
2018-05-18 14:39:54 +0800

    新增一个日志对象所在的模块

---
+++ django_logs/logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time: 2018/5/18 下午2:38
+# @Author: BlackMatrix
+# @Site: https://github.com/blackmatrix7
+# @File: logger
+# @Software: PyCharm
+import logging
+
+__author__ = 'blackmatrix'
+
+logger = logging.getLogger('django')
+
+
+if __name__ == '__main__':
+    pass
