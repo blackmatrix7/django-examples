9c124c29b435ca565ef1b9f924608a3b427722be
Matrix <codecolor@outlook.com>
2018-05-18 16:43:05 +0800

    修改app url

--- django_logs/proj/urls.py
+++ django_logs/proj/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from app import views
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test_log/', views.test_logger),
 ]
