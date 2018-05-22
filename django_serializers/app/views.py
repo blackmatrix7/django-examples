53c71b9224de99da74cce29521e85e725ba4afb8
Matrix <codecolor@outlook.com>
2018-05-22 16:26:17 +0800

    可以正常运行的代码

--- django_serializers/app/views.py
+++ django_serializers/app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.core.serializers import serialize
 
 # Create your views here.
+from .models import Book
+
+
+def get_books(requests):
+    book = Book.objects.all()
+    json_str = serialize('json', book)
+    return JsonResponse({'books': json.loads(json_str)})
+
+
+
