aadb4944c01db707784929728e89ece502694a06
Matrix <codecolor@outlook.com>
2018-05-23 17:35:39 +0800

    优化代码结构

--- django_serializers/app/models.py
+++ django_serializers/app/models.py
@@ -3,16 +3,15 @@ from django.db import models
 
 class Person(models.Model):
 
+    class Meta:
+        unique_together = (('first_name', 'last_name'),)
+
+    birthdate = models.DateField()
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    birthdate = models.DateField()
-
     def natural_key(self):
         return [self.first_name, self.last_name]
-
-    class Meta:
-        unique_together = (('first_name', 'last_name'),)
 
 
 class Book(models.Model):
--- django_serializers/app/views.py
+++ django_serializers/app/views.py
@@ -7,7 +7,7 @@ from django.core.serializers import serialize
 # Create your views here.
 def get_books(request):
     book = Book.objects.all()
-    json_str = serialize('json', book, use_natural_foreign_keys=True)
+    json_str = serialize('json', book, )
     return JsonResponse({'books': json.loads(json_str)})
 
 
