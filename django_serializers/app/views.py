d244a38fe39b1b44c5178e49cbf7636ff84e3943
Matrix <codecolor@outlook.com>
2018-05-27 13:44:02 +0800

    增加反序列化的方法，用于测试

--- django_serializers/app/views.py
+++ django_serializers/app/views.py
@@ -1,13 +1,22 @@
 import json
 from .models import Book
+from django.core import serializers
 from django.http import JsonResponse
-from django.core.serializers import serialize
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def get_books(request):
     book = Book.objects.all()
-    json_str = serialize('json', book, )
+    json_str = serializers.serialize('json', book, )
+    return JsonResponse({'books': json.loads(json_str)})
+
+
+# Create your views here.
+@csrf_exempt
+def set_books(request):
+    body = request.body.decode()
+    json_str = serializers.deserialize('json', body)
     return JsonResponse({'books': json.loads(json_str)})
 
 
