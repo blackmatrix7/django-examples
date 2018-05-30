9f341e87530f1fc7eb5ad05608c55f296f59dbc8
Matrix <codecolor@outlook.com>
2018-05-30 14:20:30 +0800

    调整表结构，增加through支持

--- django_m2m/app/models.py
+++ django_m2m/app/models.py
@@ -3,18 +3,19 @@ from django.db import models
 # Create your models here.
 
 
-class Customer(models.Model):
-
-    customer_name = models.CharField('姓名', max_length=24)
-    age = models.IntegerField('年龄')
-
-
 class Product(models.Model):
     product_name = models.CharField('商品名称', max_length=24)
     price = models.DecimalField('价格', max_digits=10, decimal_places=6)
 
 
-class Order(models.Model):
+class Customer(models.Model):
+
+    customer_name = models.CharField('姓名', max_length=24)
+    age = models.IntegerField('年龄')
+    shopping = models.ManyToManyField(Product, through='Shopping', through_fields=('customer', 'product'))
+
+
+class Shopping(models.Model):
 
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name='客户')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='商品')
