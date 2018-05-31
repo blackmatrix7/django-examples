4e1df80c05c398b87114f2183ff8e59fe8c78bb6
Matrix <codecolor@outlook.com>
2018-05-31 09:07:38 +0800

    删除冗余的views

--- django_curd/app/views.py
+++ django_curd/app/views.py
@@ -1,31 +1,6 @@
-from .models import *
-from django.http import JsonResponse
-from django.forms.models import model_to_dict
+
 
 # Create your views here.
-
-
-def customer_products(request):
-    """
-    查询某个客户所有购买过的商品和数量
-    :param request:
-    :return:
-    """
-    customer = Customer.objects.get(id=1)
-    return JsonResponse([model_to_dict(product) for product in customer.products.all()], safe=False)
-
-
-def change(request):
-    customer = Customer.objects.get(id=1)
-    # 自定义中间表时，add方法不可用，因为中间表可能会有其他字段
-    # 比如中间表Shopping增加count字段
-    # customer.products.add(1)
-    # 同样的，set方法也不可用
-    # AttributeError
-    # Cannot set values on a ManyToManyField which specifies an intermediary model. Use app.Shopping's Manager instead.
-    # customer.products.set([1, 2, 3])
-    # remove也是不可用的
-    # customer.products.remove(1)
 
 
 
--- django_curd/proj/urls.py
+++ django_curd/proj/urls.py
@@ -19,6 +19,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('change/', views.change),
-    path('customer/products/', views.customer_products),
 ]
