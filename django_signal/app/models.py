4eac43ea483c9502e88b8ed8dca0c32a9ed8f295
Matrix <codecolor@outlook.com>
2018-06-28 10:54:27 +0800

    修改model及迁移脚本

--- django_signal/app/models.py
+++ django_signal/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Article(models.Model):
+class Pizza(models.Model):
 
-    title = models.CharField('标题', max_length=128)
-    content = models.TextField('内容')
+    toppings = models.CharField('饼底', max_length=128)
+    size = models.IntegerField('尺寸')
